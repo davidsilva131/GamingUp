0fb074731de936678efbf6f8290288e5faf80ae9
David Silva <davidsilvac131@gmail.com>
2023-06-01 19:52:00 -0500

    Add constants file

---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+import { Dancing_Script } from 'next/font/google'
+
+export const LINKS = [
+  {
+    text: 'Inicio',
+    href: '/'
+  },
+  {
+    text: 'Equipos',
+    href: '/equipos'
+  },
+  {
+    text: 'Tienda',
+    href: '/tienda'
+  }
+]
+
+export const dancing = Dancing_Script({ subsets: ['latin'] })
