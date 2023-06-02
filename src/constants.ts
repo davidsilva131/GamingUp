905f7f94ddf9034da748a32e269d5387909af484
David Silva <davidsilvac131@gmail.com>
2023-06-02 15:17:26 -0500

    Add socials

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,9 @@
 import { Dancing_Script } from 'next/font/google'
+import InstagramLogo from '@/assets/instagram.svg'
+import TwitterLogo from '@/assets/twitter.svg'
+import TiktokLogo from '@/assets/tiktok.svg'
+import TwitchLogo from '@/assets/twitch.svg'
+import DiscordLogo from '@/assets/discord.svg'
 
 export const LINKS = [
   {
@@ -16,3 +21,31 @@ export const LINKS = [
 ]
 
 export const dancing = Dancing_Script({ subsets: ['latin'] })
+
+export const SOCIALS = [
+  {
+    socialName: 'Instagram',
+    image: InstagramLogo,
+    link: 'https://www.instagram.com/teamgamingup/'
+  },
+  {
+    socialName: 'Twitter',
+    image: TwitterLogo,
+    link: 'https://twitter.com/teamgamingup'
+  },
+  {
+    socialName: 'Tiktok',
+    image: TiktokLogo,
+    link: 'https://www.tiktok.com/@teamgamingup'
+  },
+  {
+    socialName: 'Twitch',
+    image: TwitchLogo,
+    link: 'https://www.twitch.tv/gamingupteam'
+  },
+  {
+    socialName: 'Discord',
+    image: DiscordLogo,
+    link: 'https://discord.com/invite/j5UmVWJ3'
+  }
+]
